62a3e57c6a948336bef9e23ffe36a91f9a459841
Vadim Zgerskiy <vadzim.zgerski@instinctools.ru>
2019-04-08 14:27:34 +0300

    Fix remarks

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-// window.addEventListener('load', function () {
-//     const firstSection = window.document.querySelector('.image');
-//     console.log(firstSection);
-//     console.log(123, firstSection.offsetTop);
-// });
 window.addEventListener('scroll', function () {
     const minHeght = 544;
     const firstSection = window.document.querySelector('.image');
-    const currentContentHeight = firstSection.offsetHeight;
     firstSection.style.height = `${window.scrollY + minHeght}px`;
     firstSection.style.minHeight = minHeght + 'px';
-    console.log(11,currentContentMinHeight);
 });
