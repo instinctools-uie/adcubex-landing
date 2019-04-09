8faf34bdb9646adb94aa3e4ed5fad2194f088747
Dasha <kokorina.darya1509@gmail.com>
2019-04-09 12:16:54 +0300

    click menu fix

--- menu.js
+++ menu.js
@@ -2,9 +2,12 @@ const items = document.querySelectorAll('.menu-container > span');
 
 items.forEach((item) => {
     item.addEventListener('click', function (e) {
-        items.forEach(item => {
+        items.forEach((item, index, arr) => {
             if (item.style.color === 'red') {
-                item.style.color = 'black';
+                if (index === 0) {
+                    item.style.color = arr[index+1].style.color;
+                }
+                item.style.color = arr[index-1].style.color;
             }
         });
         e.target.style.color = 'red';
