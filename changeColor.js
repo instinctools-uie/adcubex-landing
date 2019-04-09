b175fceffe2cae676b5dc033a24688a855901e34
Dasha <kokorina.darya1509@gmail.com>
2019-04-09 14:55:22 +0300

    fixes after MR#2

--- changeColor.js
+++ changeColor.js
@@ -22,34 +22,39 @@ function fromRedToWhiteLogo(){
     const sourceColor = {red: 237, green: 28, blue: 36};
     const perChange = (window.pageYOffset - 960) / 960;
     const currentColor = {red: Math.round(sourceColor.red + (18 * perChange)), green: Math.round(sourceColor.green + (227 * perChange)), blue: Math.round(sourceColor.blue + (219 * perChange))};
-    document.querySelector('.logo-container > svg').style.fill = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+    const logo = document.querySelector('.logo-container > svg');
+    logo.style.fill = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
 }
 
 function fromBlackToWhiteMenu(){
     const perChange = (window.pageYOffset - 960) / 960;
     const currentColor = {red: Math.round(255 * perChange), green: Math.round(255 * perChange), blue: Math.round(255 * perChange)};
-    document.querySelector('.menu-container').style.color = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
-    document.querySelector('.menu-button > svg').style.fill = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+    const menu = document.querySelector('.menu-container');
+    menu.style.color = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+    const menButton = document.querySelector('.menu-button > svg');
+    menButton.style.fill = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
 }
+
+const container = document.querySelector('body');
 
 function fromWhiteToRed(){
     const sourceColor = {red: 255, green: 255, blue: 255};
     const perChange = 1 - ((window.pageYOffset - 960) / 960);
     const currentColor = {red: sourceColor.red, green: Math.round(sourceColor.green * perChange), blue: Math.round(sourceColor.blue * perChange)};
-        document.querySelector('body').style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+    container.style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
 }
 
 function fromRedToBlue(){
     const sourceColor = {red: 255, green: 0, blue: 0};
     const perChange = (window.pageYOffset - 1920) / 960;
     const currentColor = {red: Math.round(sourceColor.red * (1 - perChange)), green: 0, blue: Math.round(255 * perChange)};
-        document.querySelector('body').style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+    container.style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
 }
 
 function fromBlueToPurple(){
     const sourceColor = {red: 0, green: 0, blue: 255};
     const perChange = (window.pageYOffset - 2880) / 960;
     const currentColor = {red: Math.round(157 * perChange), green: Math.round(55 * perChange), blue: Math.round(sourceColor.blue - (28 * perChange))};
-        document.querySelector('body').style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
+    container.style.backgroundColor = `rgb(${currentColor.red}, ${currentColor.green}, ${currentColor.blue})`;
 }
 
--- menu.js
+++ menu.js
@@ -1,20 +1,14 @@
 const menuContainer = document.querySelector('.menu-container');
 menuContainer.addEventListener('click', function (e) {
-    const items = document.querySelectorAll('.menu-container > span');
+        const items = document.querySelectorAll('.menu-container > span');
 
-    for (let i = 0; i < items.length; i++) {
-        const isActiveLink = items[i].classList.contains('link--active');
-        if (isActiveLink && i !== 0) {
-            items[i].classList.remove('link--active');
-            items[i].style.color = items[i-1].style.color;
+        for (let i = 0; i < items.length; i++) {
+            const isActiveLink = items[i].classList.contains('link--active');
+            if (isActiveLink && i !== 0) {
+                items[i].classList.remove('link--active');
+            }
+            e.target.classList.add('link--active');
+
         }
-
-        if (isActiveLink && i === 0){
-            items[i].classList.remove('link--active');
-            items[i].style.color = items[i+1].style.color;
-        }
-    }
-        e.target.classList.add('link--active');
-
     }
 );
