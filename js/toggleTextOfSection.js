ead9d49eee373bf2de6d3032ade85bf76e1f47f6
Dasha <kokorina.darya1509@gmail.com>
2019-04-15 13:13:31 +0300

    scaleFix

--- js/toggleTextOfSection.js
+++ js/toggleTextOfSection.js
@@ -1,18 +1,23 @@
 function toggleTextOfSection() {
-  const scrolled = document.scrollingElement.scrollTop;
   const sections = window.document.querySelectorAll('.section-container');
+  if (window.innerHeight <= 1320) {
+    for (let i = 0; i < sections.length; i++) {
+      const sectionCoordinates = sections[i].getBoundingClientRect();
 
-  for (let i = 0; i < sections.length; i++) {
-    const position = sections[i].offsetTop;
-    const positionOfScrollBeforeSection = position - 600;
-    const positionOfScrollAfterSection = position + 350;
-    const isScrollOnSection = scrolled > positionOfScrollBeforeSection && scrolled < positionOfScrollAfterSection;
-    const isActiveSection = sections[i].classList.contains('section-container--active');
+      const isScrollOnSection = sectionCoordinates.top >= 0 && sectionCoordinates.top <= 0.87 * window.innerHeight;
+      const isActiveSection = sections[i].classList.contains('section-container--active');
 
-    if (isScrollOnSection && !isActiveSection) {
+      if (isScrollOnSection && !isActiveSection) {
+        sections[i].classList.add('section-container--active');
+      } else if (!isScrollOnSection && isActiveSection) {
+        sections[i].classList.remove('section-container--active');
+      }
+    }
+  }
+
+  if (window.innerHeight > 1320) {
+    for (let i = 0; i < sections.length; i++) {
       sections[i].classList.add('section-container--active');
-    } else if (!isScrollOnSection && isActiveSection) {
-      sections[i].classList.remove('section-container--active');
     }
   }
 }
