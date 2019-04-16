a152260f0094fd9a6b74559ec5c0fa5e334e4b34
Dasha <kokorina.darya1509@gmail.com>
2019-04-16 10:51:38 +0300

    add square

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ var advantageSectionScrollPosition = advantageSection.offsetTop;
 var expertiseSectionScrollPosition = expertiseSection.offsetTop;
 var investmentsSectionScrollPosition = investmentsSection.offsetTop;
 
+const sections = window.document.querySelectorAll('.section-container');
+for (let i = 0; i < sections.length; i++) {
+  sections[i].classList.add('section-container--active');
+}
+
 window.addEventListener('scroll', initProject);
 
 window.addEventListener('load', initProject);
