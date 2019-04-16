a1767a97d421b14d0c8267909ac2f9e04415c079
Vadim Zgerskiy <vadzim.zgerski@instinctools.ru>
2019-04-16 18:20:40 +0300

    Added webpack, eslint, tests

--- src/js/toggleTextOfSection.js
+++ src/js/toggleTextOfSection.js
@@ -1,25 +1,24 @@
-//TODO: Need find solution for correctly toggle text when zoom
-(function(window) {
-  window.AdCubex.toggleTextOfSection = function() {
-    const scrolled = window.pageYOffset;
-    const sections = window.document.querySelectorAll('.section-container');
+//  TODO: Need find solution for correctly toggle text when zoom
 
-    for (let i = 0; i < sections.length; i++) {
-      const currentSection = sections[i];
-      const nextSection = sections[i + 1];
-      const scrollPositionOfCurrentSection = currentSection.offsetTop;
-      const scrollPositionOfNextSection = nextSection ? nextSection.offsetTop : document.documentElement.scrollHeight;
-      const scrollDifferenceBetweenSections = scrollPositionOfNextSection - scrollPositionOfCurrentSection;
-      const scrollHalfOfDifferenceBetweenSections = scrollDifferenceBetweenSections / 2;
-      const scrollPositionWhenShouldToggleText = scrollHalfOfDifferenceBetweenSections + scrollPositionOfCurrentSection;
+export function toggleTextOfSection() {
+  const scrolled = window.pageYOffset;
+  const sections = window.document.querySelectorAll('.section-container');
 
-      if (scrolled > scrollPositionWhenShouldToggleText && scrolled < scrollPositionOfNextSection) {
-        nextSection.classList.add('section-container--active');
-        currentSection.classList.remove('section-container--active');
-      } else if (scrolled < scrollPositionWhenShouldToggleText && scrolled > scrollPositionOfCurrentSection) {
-        currentSection.classList.add('section-container--active');
-        nextSection.classList.remove('section-container--active');
-      }
+  for (let i = 0; i < sections.length; i++) {
+    const currentSection = sections[i];
+    const nextSection = sections[i + 1];
+    const scrollPositionOfCurrentSection = currentSection.offsetTop;
+    const scrollPositionOfNextSection = nextSection ? nextSection.offsetTop : document.documentElement.scrollHeight;
+    const scrollDifferenceBetweenSections = scrollPositionOfNextSection - scrollPositionOfCurrentSection;
+    const scrollHalfOfDifferenceBetweenSections = scrollDifferenceBetweenSections / 2;
+    const scrollPositionWhenShouldToggleText = scrollHalfOfDifferenceBetweenSections + scrollPositionOfCurrentSection;
+
+    if (scrolled > scrollPositionWhenShouldToggleText && scrolled < scrollPositionOfNextSection) {
+      nextSection.classList.add('section-container--active');
+      currentSection.classList.remove('section-container--active');
+    } else if (scrolled < scrollPositionWhenShouldToggleText && scrolled > scrollPositionOfCurrentSection) {
+      currentSection.classList.add('section-container--active');
+      nextSection.classList.remove('section-container--active');
     }
-  };
-})(window);
+  }
+}
