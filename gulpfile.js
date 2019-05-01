83481c7c1bf49d7e9c3340a0e5a3953ca65b35d8
Dasha <kokorina.darya1509@gmail.com>
2019-05-01 16:04:57 +0300

    create separate js file for the page

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const buffer = require('vinyl-buffer');
 const mainBowerFiles = require('main-bower-files');
 const image = require('gulp-image');
 
-const jsFolders = ['mainPage', 'innerPage'];
+const jsFolders = ['mainPage', 'innerPage', 'contactPage'];
 const cssFolders = ['mainPage', 'synergyPage', 'strategyPage', 'solutionsPage', 'contactPage'];
 
 gulp.task('connect', function() {
