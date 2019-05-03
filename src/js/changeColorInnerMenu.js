b8d2c90e7138f0f1a93b5c18f3166b41504bf241
Dasha <kokorina.darya1509@gmail.com>
2019-05-03 17:06:57 +0300

    fix about number

--- src/js/changeColorInnerMenu.js
+++ src/js/changeColorInnerMenu.js
@@ -3,16 +3,19 @@ const contentSection = document.querySelector('.content-section');
 const menuButton = document.querySelector('.header-open-menu-icon');
 const logo = document.querySelector('#Logo_dark_curves');
 
+const widthToChangeMenuColor = 1730;
+const menuCrossContentSection = 70;
+
 function changeMenuColor() {
   const viewWidth = document.documentElement.clientWidth;
   const topCoordinate = contentSection.getBoundingClientRect().top;
 
-  if (viewWidth < 1730 && topCoordinate < 70) {
+  if (viewWidth < widthToChangeMenuColor && topCoordinate < menuCrossContentSection) {
     menuButton.style.fill = '#353535';
     logo.style.fill = '#353535';
   }
 
-  if (topCoordinate > 70 || viewWidth > 1730) {
+  if (topCoordinate > menuCrossContentSection || viewWidth > widthToChangeMenuColor) {
     menuButton.style.fill = '#ffffff';
     logo.style.fill = '#ffffff';
   }
