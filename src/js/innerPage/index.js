c983d570e9894fe45cdfe5d1e1b352b4de82a7fe
Dasha <kokorina.darya1509@gmail.com>
2019-05-13 17:04:17 +0300

    titles2

--- src/js/innerPage/index.js
+++ src/js/innerPage/index.js
@@ -1,7 +1,12 @@
 import toggleMenu from '../toggleMenu';
 import hoverLinkInMenu from '../hoverLinkInMenu';
 import changeMenuColor from './changeColorInnerMenu';
+import toggleSections from './toggleInnerTextOfArticles';
 
 toggleMenu();
 hoverLinkInMenu();
 changeMenuColor();
+
+window.addEventListener('scroll', () => {
+  toggleSections(20);
+});
