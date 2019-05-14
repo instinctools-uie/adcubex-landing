57a33220414eca9628917ec83257a0475bd14957
Dasha <kokorina.darya1509@gmail.com>
2019-05-14 11:37:09 +0300

    refactor inner pages toggleMethod call

--- src/js/innerPage/index.js
+++ src/js/innerPage/index.js
@@ -2,11 +2,12 @@ import toggleMenu from '../toggleMenu';
 import hoverLinkInMenu from '../hoverLinkInMenu';
 import changeMenuColor from './changeColorInnerMenu';
 import toggleSections from './toggleInnerTextOfArticles';
+import { INNER_PAGE_DELAY_BETWEEN_SECTION } from '../constants';
 
 toggleMenu();
 hoverLinkInMenu();
 changeMenuColor();
 
 window.addEventListener('scroll', () => {
-  toggleSections(20);
+  toggleSections(INNER_PAGE_DELAY_BETWEEN_SECTION);
 });
