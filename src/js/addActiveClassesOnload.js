17111a5267a2d1e820918b8e734f4b1ed16a7952
Dasha <kokorina.darya1509@gmail.com>
2019-05-17 17:05:39 +0300

    fix animation on inner pages

--- src/js/addActiveClassesOnload.js
+++ src/js/addActiveClassesOnload.js
@@ -2,7 +2,7 @@ export default function addActiveSections(section) {
   const sections = window.document.querySelectorAll(`.${section}`);
   let currentSectionIndex = null;
   sections.forEach((item, index) => {
-    if (item.getBoundingClientRect().top > -26 && item.getBoundingClientRect().top < window.innerHeight) {
+    if (item.getBoundingClientRect().top > -350 && item.getBoundingClientRect().top < window.innerHeight) {
       currentSectionIndex = index;
     }
   });
--- src/js/innerPage/index.js
+++ src/js/innerPage/index.js
@@ -2,11 +2,16 @@ import toggleMenu from '../toggleMenu';
 import hoverLinkInMenu from '../hoverLinkInMenu';
 import changeMenuColor from './changeColorInnerMenu';
 import toggleContentByScroll from '../toggleContentByScroll';
+import addActiveSections from '../addActiveClassesOnload';
 import { INNER_PAGES_TOGGLE_CONTENT } from '../constants';
 
 toggleMenu();
 hoverLinkInMenu();
 changeMenuColor();
+
+window.addEventListener('load', () => {
+  addActiveSections(INNER_PAGES_TOGGLE_CONTENT.sectionContainer);
+});
 
 window.addEventListener('scroll', () => {
   toggleContentByScroll(INNER_PAGES_TOGGLE_CONTENT.sectionContainer, INNER_PAGES_TOGGLE_CONTENT.delayBetweenSection);
